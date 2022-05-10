c83639cd446a7c4ce8a24a08316f904040f2f151
aandreea <A.Zlei@student.tudelft.nl>
2022-05-10 21:02:10 +0200

    add folder structure and template application

--- mainClient/src/App.js
+++ mainClient/src/App.js
@@ -1,94 +1,33 @@
-import logo from './logo.svg';
 import React from 'react';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
-
-class Joke extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      liked: ""
-    }
-    this.like = this.like.bind(this);
-  }
-
-  like() {
-    // ... we'll add this block later
-  }
-
-  render() {
-    return (
-        <div className="col-xs-4">
-          <div className="panel panel-default">
-            <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
-            <div className="panel-body">
-              {this.props.joke.joke}
-            </div>
-            <div className="panel-footer">
-              {this.props.joke.likes} Likes &nbsp;
-              <a onClick={this.like} className="btn btn-default">
-                <span className="glyphicon glyphicon-thumbs-up"></span>
-              </a>
-            </div>
-          </div>
-        </div>
-    )
-  }
-}
-
-class HelloWorld extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("http://localhost:8080/helloWorldJson")
-        .then(res => res.json())
-        .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                items: result
-              });
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            },
-        )
-  }
-
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-          this.state.items.map(function(joke, i){
-              return (<Joke key={i} joke={joke} />);
-            })
-      );
-    }
-  }
-
-}
+import './Components/Article/Article.js';
+import Homepage from './Components/Homepage/Homepage.js';
+import FileUpload from './Components/Repo/FileUpload.js';
+import ArticleList from "./Components/Article/ArticleList";
+import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HelloWorld/>
-    </div>
+      <Router>
+          <div className="App d-flex flex-column min-vh-100">
+              <Header/>
+              <Routes>
+                  <Route exact path='/' element={<Homepage/>}></Route>
+                  <Route exact path='/papers' element={<ArticleList/>}></Route>
+                  <Route exact path='/upload' element={<FileUpload/>}></Route>
+              </Routes>
+              <Footer/>
+          </div>
+      </Router>
   );
 }
 
