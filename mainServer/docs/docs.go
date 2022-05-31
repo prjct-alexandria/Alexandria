2e18a301a6b1890b09823919c694d1368e439657
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2022-05-31 16:23:46 +0200

    Add body paramaters for authentication in swagger docs

--- mainServer/docs/docs.go
+++ mainServer/docs/docs.go
@@ -214,6 +214,17 @@ const docTemplate = `{
                     "application/json"
                 ],
                 "summary": "Endpoint for user logging in",
+                "parameters": [
+                    {
+                        "description": "User credentials",
+                        "name": "credentials",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/models.LoginForm"
+                        }
+                    }
+                ],
                 "responses": {
                     "200": {
                         "description": "Success"
@@ -237,6 +248,17 @@ const docTemplate = `{
                     "application/json"
                 ],
                 "summary": "Endpoint for user registration",
+                "parameters": [
+                    {
+                        "description": "User credentials",
+                        "name": "credentials",
+                        "in": "body",
+                        "required": true,
+                        "schema": {
+                            "$ref": "#/definitions/entities.User"
+                        }
+                    }
+                ],
                 "responses": {
                     "200": {
                         "description": "Success"
@@ -296,6 +318,21 @@ const docTemplate = `{
                     }
                 },
                 "title": {
+                    "type": "string"
+                }
+            }
+        },
+        "models.LoginForm": {
+            "type": "object",
+            "required": [
+                "email",
+                "pwd"
+            ],
+            "properties": {
+                "email": {
+                    "type": "string"
+                },
+                "pwd": {
                     "type": "string"
                 }
             }
