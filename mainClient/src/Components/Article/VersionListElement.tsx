b64af2278bca4f3c5ed144b6673ffb39e6e3ea30
Amy van der Meijden <a.vandermeijden@student.tudelft.nl>
2022-06-01 09:42:10 +0200

    Resolve "List merge requests per article"

--- mainClient/src/Components/Article/VersionListElement.tsx
+++ mainClient/src/Components/Article/VersionListElement.tsx
@@ -12,16 +12,22 @@ type VListProps = {
 
 export default function VersionListElement(props: VListProps) {
   let params = useParams();
+  let baseLink = '/articles/' + params.aid + '/requests'
+
   return (
     <div className="row row-no-gutters col-md-12 text-wrap">
-      <div className="col-md-8 text-start">
+      <div className="col-md-4 text-start">
         <Link
           to={"/articles/" + params.articleId + "/versions/" + props.version.versionID}
         >
           {props.version.title}
         </Link>
       </div>
-      <div className="col-md-3">{props.version.owners.join(', ')}</div>
+      <div className="col-md-2"><Link to={baseLink}>See all requests</Link></div>
+      <div className="col-md-2"><Link to={baseLink + '?source=' + props.version.versionID}>See requests as source</Link></div>
+      <div className="col-md-2"><Link to={baseLink + '?target=' + props.version.versionID}>See requests as target</Link></div>
+
+      <div className="col-md-1">{props.version.owners.join(', ')}</div>
       <div className="col-md-1">{props.version.status}</div>
     </div>
   );
