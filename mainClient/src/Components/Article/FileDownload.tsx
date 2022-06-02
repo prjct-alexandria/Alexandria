ed8a0c44f19522c390d5be3105f2a9d3202689ad
aandreea <A.Zlei@student.tudelft.nl>
2022-06-02 18:10:21 +0200

    Add error and success alerts

--- mainClient/src/Components/Article/FileDownload.tsx
+++ mainClient/src/Components/Article/FileDownload.tsx
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as React from "react";
+import SuccessAlert from "../SuccessAlert";
+import ErrorAlert from "../ErrorAlert";
 
 export default function FileDownload() {
-  let [isLoaded, setLoaded] = useState(false);
   let [error, setError] = useState(null);
-  let [fileZip, setFileZip] = useState<Blob | MediaSource>();
+  let [downloadSuccess, setDownloadSuccess] = useState<boolean>(false);
+  let [filename, setFilename] = useState("");
+
+  // Make url for request to access ../files endpoint
+  let params = useParams();
+  const url = "/source-file.txt";
+  // "http://localhost:8080/articles/" +
+  // params.articleId +
+  // "/versions/" +
+  // params.versionId +
+  // "/files";
 
   // Handler triggered on Download button click
-  const downloadFileHandler = () => {
-    // Make url for request to access ../files endpoint
-    let params = useParams();
-    const url =
-      "http://localhost:8080/articles/" +
-      params.articleId +
-      "/versions/" +
-      params.versionId +
-      "/files";
-
+  const downloadFileHandler = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
     // GET request to get the files as a ZIP
     fetch(url, {
       method: "GET",
@@ -32,31 +35,57 @@ export default function FileDownload() {
       .then(
         // Process the response as a BLOB (Binary large object)
         (result) => {
-          setLoaded(true);
-          setFileZip(result);
-
+          // Put the file in the DOM
           const url = window.URL.createObjectURL(result);
+          // Placeholder filename
+          setFilename("source-file.txt");
+
+          // Add a hidden <a> element to DOM, that downloads the file when clicking on it
           const a = document.createElement("a");
           a.style.display = "none";
           a.href = url;
-
-          // the filename
-          a.download = "source.zip";
+          a.download = filename;
           document.body.appendChild(a);
+
+          // Simulate clicking on <a> element to trigger download of file
           a.click();
+
+          // Remove <a> from DOM
           window.URL.revokeObjectURL(url);
-          alert("your file has downloaded!"); // replace by something with better UX
+
+          // Set success in state to show success alert
+          setDownloadSuccess(true);
+
+          // After 3s, remove success from state to hide success alert
+          setTimeout(() => setDownloadSuccess(false), 3000);
         },
         (error) => {
-          setLoaded(true);
           setError(error);
         }
       );
   };
 
   return (
-    <button className="btn btn-primary" onClick={downloadFileHandler}>
-      Download source files
-    </button>
+    <div>
+      <form>
+        <button className="btn btn-primary" onClick={downloadFileHandler}>
+          Download source files
+        </button>
+      </form>
+      {downloadSuccess && (
+        <SuccessAlert
+          title="Download successful! "
+          message={
+            "The source file " + filename + " has been successfully downloaded."
+          }
+        />
+      )}
+      {error && (
+        <ErrorAlert
+          title={"Error: "}
+          message={"Something went wrong, please try again."}
+        />
+      )}
+    </div>
   );
 }
