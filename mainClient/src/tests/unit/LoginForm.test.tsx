c0982f61f60b443dcd80d219c7ce09a3cfa76b83
aandreea <A.Zlei@student.tudelft.nl>
2022-06-02 16:15:36 +0200

    Add comments

--- mainClient/src/tests/unit/LoginForm.test.tsx
+++ mainClient/src/tests/unit/LoginForm.test.tsx
@@ -29,11 +29,11 @@ function renderLoginForm(props: Partial<LoginFormProps> = {}) {
 
 describe("<LoginForm />", () => {
   test("Display a blank login form", async () => {
-    // Asynchroniously find element by given attribute
+    //Arrange: Asynchroniously find element by given attribute
     const { findByTestId } = renderLoginForm();
     const loginForm = await findByTestId("login-form");
 
-    // Expect form to have default values
+    // Assert: Expect form to have default values
     expect(loginForm).toHaveFormValues({
       email: "",
       password: "",
@@ -41,6 +41,7 @@ describe("<LoginForm />", () => {
   });
 
   test("Enter an email", async () => {
+    //Arrange
     // Mock the onChangeEmail
     const onChangeEmail = jest.fn();
 
@@ -48,32 +49,32 @@ describe("<LoginForm />", () => {
     const { findByTestId } = renderLoginForm({ onChangeEmail });
     const email = await findByTestId("email");
 
-    // Modify form state to have the given value in the email field
+    // Act: Modify form state to have the given value in the email field
     fireEvent.change(email, { target: { value: "user@gmail.com" } });
 
-    // Expect the onChangeEmail to have been called with the given value
+    // Assert: Expect the onChangeEmail to have been called with the given value
     expect(onChangeEmail).toHaveBeenCalled();
     expect(email).toHaveValue("user@gmail.com");
   });
 
   test("Enter a password", async () => {
-    // Mock the onChangePassword
+    // Arrange: Mock the onChangePassword
     const onChangePassword = jest.fn();
 
     // Asynchroniously find element by given attribute
     const { findByTestId } = renderLoginForm({ onChangePassword });
     const password = await findByTestId("password");
 
-    // Modify form state to have the given value in the password field
+    // Act: Modify form state to have the given value in the password field
     fireEvent.change(password, { target: { value: "password" } });
 
-    // Expect the onChangePassword to have been called with the given value
+    // Assert: Expect the onChangePassword to have been called with the given value
     expect(onChangePassword).toHaveBeenCalled();
     expect(password).toHaveValue("password");
   });
 
   test("Submit form", async () => {
-    // Mock the submitHandler
+    // Arrange: Mock the submitHandler
     const submitHandler = jest.fn();
 
     // Asynchroniously find elements by given attribute
@@ -85,6 +86,7 @@ describe("<LoginForm />", () => {
     const password = await findByTestId("password");
     const submit = await findByTestId("submit");
 
+    //Act
     // Modify form state to have the given value in the email field
     fireEvent.change(email, { target: { value: "user@gmail.com" } });
 
@@ -94,7 +96,7 @@ describe("<LoginForm />", () => {
     // Modify form state to click the submit button
     fireEvent.click(submit);
 
-    // Expect the submitHandler to have been called with the given values
+    // Assert: Expect the submitHandler to have been called with the given values
     expect(submitHandler).toHaveBeenCalled();
     expect(email).toHaveValue("user@gmail.com");
     expect(password).toHaveValue("password");
