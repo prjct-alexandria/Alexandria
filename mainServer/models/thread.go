582fd4662a3b879892ec1d37627d97caecb919a8
Jos Sloof <a.j.g.sloof@student.tudelft.nl>
2022-06-07 14:24:43 +0200

    add json annotations

--- mainServer/models/thread.go
+++ mainServer/models/thread.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Thread struct {
-	Id         int64
-	ArticleId  int64
-	SpecificId int64
-	Comment    []entities.Comment
+	Id         int64              `json:"id"`
+	ArticleId  int64              `json:"articleId"       binding:"required"`
+	SpecificId int64              `json:"specificId"      binding:"required"`
+	Comment    []entities.Comment `json:"comment"         binding:"required"`
 }
 
 type ReturnThreadIds struct {
-	Id        int64
-	ThreadId  int64
-	CommentId int64
+	Id        int64 `json:"id"              binding:"required"`
+	ThreadId  int64 `json:"threadId"        binding:"required"`
+	CommentId int64 `json:"CommentId"       binding:"required"`
 }
