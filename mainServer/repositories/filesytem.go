74e89ac67df98c13c456a4de39778e0ddccc81c5
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2022-06-15 12:21:21 +0200

    Split up method with high complexity

--- mainServer/repositories/filesytem.go
+++ mainServer/repositories/filesytem.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"archive/zip"
 	"io"
+	"io/fs"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -27,38 +28,39 @@ func (repo FilesystemRepository) AddFilesInDirToZip(zipWriter *zip.Writer, dirPa
 
 	for _, file := range files {
 		// Wrapped in function to allow for "defer file.close()"
-		err := func() error {
-			if file.IsDir() {
-				//Check if it is not the git folder
-				if file.Name() != ".git" {
-					err := repo.AddFilesInDirToZip(zipWriter, filepath.Join(dirPath, file.Name()), file.Name())
-					if err != nil {
-						return err
-					}
-				}
-			} else {
-				//Create file in zip
-				zipFile, err := zipWriter.Create(filepath.Join(dirInZip, file.Name()))
-				if err != nil {
-					return err
-				}
+		err := repo.addFileInDirToZip(file, zipWriter, dirPath, dirInZip)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
-				//Open file on branch
-				fileReader, err := os.Open(filepath.Join(dirPath, file.Name()))
-				if err != nil {
-					return err
-				}
-				defer fileReader.Close()
-
-				//Copy contents over
-				_, err = io.Copy(zipFile, fileReader)
-				if err != nil {
-					return err
-				}
+func (repo FilesystemRepository) addFileInDirToZip(file fs.FileInfo, zipWriter *zip.Writer, dirPath string, dirInZip string) error {
+	if file.IsDir() {
+		//Check if it is not the git folder
+		if file.Name() != ".git" {
+			err := repo.AddFilesInDirToZip(zipWriter, filepath.Join(dirPath, file.Name()), file.Name())
+			if err != nil {
+				return err
 			}
-			return nil
-		}()
+		}
+	} else {
+		//Create file in zip
+		zipFile, err := zipWriter.Create(filepath.Join(dirInZip, file.Name()))
+		if err != nil {
+			return err
+		}
 
+		//Open file on branch
+		fileReader, err := os.Open(filepath.Join(dirPath, file.Name()))
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		//Copy contents over
+		_, err = io.Copy(zipFile, fileReader)
 		if err != nil {
 			return err
 		}
