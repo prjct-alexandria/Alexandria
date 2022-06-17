6661fa83360d08ca19574c6c5b8d65fa277dd418
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2022-06-17 12:14:28 +0200

    Add getting commitSectionThreads in backend

--- mainServer/repositories/postgres/commitSectionThreadRepository.go
+++ mainServer/repositories/postgres/commitSectionThreadRepository.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
+	"mainServer/entities"
 	"mainServer/models"
 )
 
@@ -48,5 +49,71 @@ func (r PgCommitSectionThreadRepository) CreateCommitSectionThread(cid string, t
 }
 
 func (r PgCommitSectionThreadRepository) GetCommitSectionThreads(aid int64, cid string) ([]models.SectionThread, error) {
-	return nil, nil
+	// construct list of threads
+	stmt, err := r.Db.Prepare(`SELECT t.threadid, articleid, commitid, commentid, authorid, creationdate, content, section
+	FROM commitsectionthread ct JOIN thread t on ct.threadid = t.threadid JOIN comment c on t.threadid = c.threadid
+	WHERE t.articleid = $1 AND ct.commitid = $2 ORDER BY creationdate`)
+
+	if err != nil {
+		return nil, fmt.Errorf("GetCommitThreads: %v", err)
+	}
+
+	rows, err := stmt.Query(aid, cid)
+	if err != nil {
+		return nil, fmt.Errorf("GetCommitThreads: %v", err)
+	}
+	defer rows.Close()
+	if err != nil {
+		return nil, fmt.Errorf("GetCommitThreads: %v", err)
+	}
+
+	var threads []models.SectionThread
+	for rows.Next() {
+		// declare variables
+		var tid int64
+		var aid int64
+		var cid string
+		var coid int64
+		var auid string
+		var cd string
+		var c string
+		var section string
+		if err := rows.Scan(&tid, &aid, &cid, &coid, &auid, &cd, &c, &section); err != nil {
+			return threads, fmt.Errorf("GetCommitThreads: %v", err)
+		}
+		// find index of tid in the threads list
+		index := -1
+		for i, v := range threads {
+			if v.Id == tid {
+				index = i
+				break
+			}
+		}
+		// construct comment
+		comment := entities.Comment{
+			Id:           coid,
+			AuthorId:     auid,
+			ThreadId:     tid,
+			Content:      c,
+			CreationDate: cd,
+		}
+		// if index tid isn't yet in the list, add new thread, else add comment to thread
+		if index == -1 {
+			var comments []entities.Comment
+			comments = append(comments, comment)
+			threads = append(threads, models.SectionThread{
+				Id:         tid,
+				ArticleId:  aid,
+				SpecificId: cid,
+				Comment:    comments,
+				Section:    section,
+			})
+		} else {
+			threads[index].Comment = append(threads[index].Comment, comment)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return threads, fmt.Errorf("GetCommitThreads: %v", err)
+	}
+	return threads, nil
 }
