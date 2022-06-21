cf3c2f13c81ea9c6ca64bc3572cfe6e256861225
Emiel Witting <e.a.witting@student.tudelft.nl>
2022-06-21 23:47:20 +0200

    store subfolder paths in filesystem struct

--- mainServer/repositories/storer/filesystem/filesystem.go
+++ mainServer/repositories/storer/filesystem/filesystem.go
@@ -12,32 +12,39 @@ import (
 )
 
 type FileSystem struct {
-	path        string
-	defaultFile string
+	path         string
+	articlePath  string
+	requestPath  string
+	downloadPath string
+	defaultFile  string
 }
 
 // NewFileSystem creates file system struct and creates folders
 func NewFileSystem(path string, defaultFile string) *FileSystem {
 
-	// make folder for git repos
-	err := os.MkdirAll(filepath.Join(path, "persistent", "articles"), os.ModePerm)
+	// Create filesystem struct with paths
+	fs := &FileSystem{
+		path:         path,
+		articlePath:  filepath.Join(path, "persistent", "articles"),
+		requestPath:  filepath.Join(path, "persistent", "requests"),
+		downloadPath: filepath.Join(path, "cache", "downloads"),
+		defaultFile:  defaultFile}
+
+	// Make all folder if they don't exist yet
+	err := os.MkdirAll(fs.articlePath, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	err = os.MkdirAll(fs.requestPath, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	err = os.MkdirAll(fs.downloadPath, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 
-	// make folder for request before-and-afters
-	err = os.MkdirAll(filepath.Join(path, "persistent", "requests"), os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	// make (cache) folder for download zip's
-	err = os.MkdirAll(filepath.Join(path, "cache", "downloads"), os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	return &FileSystem{path: path, defaultFile: defaultFile}
+	return fs
 }
 
 // CreateArticlePath makes a new folder for an article git repository, returns the path.
@@ -65,14 +72,14 @@ func (fs FileSystem) CreateArticlePath(article int64) (string, error) {
 // GetArticlePath returns the path to an article git repository, does not check if it exists
 func (fs FileSystem) GetArticlePath(article int64) (string, error) {
 	idString := strconv.FormatInt(article, 10)
-	path, err := filepath.Abs(filepath.Join(fs.path, "persistent", idString))
+	path, err := filepath.Abs(filepath.Join(fs.articlePath, idString))
 	if err != nil {
 		return "", err
 	}
 	return filepath.Clean(path), err
 }
 
-// GetDownloadPath returns the path that a (temporary) dow
+// GetDownloadPath returns the path that a (temporary) zip for downloading can be stored in
 func (fs FileSystem) GetDownloadPath(filename string) (string, error) {
 	// check that the filename does not contain illegal characters
 	if !strings.Contains(IllegalChars, filename) {
@@ -80,7 +87,7 @@ func (fs FileSystem) GetDownloadPath(filename string) (string, error) {
 	}
 
 	// create the filepath, absolute and cleaned
-	path := filepath.Join(fs.path, "cache", "downloads", filename+".zip")
+	path := filepath.Join(fs.downloadPath, filename+".zip")
 	path, err := filepath.Abs(path)
 	if err != nil {
 		return "", err
@@ -94,7 +101,7 @@ func (fs FileSystem) GetRequestPath(article int64, request int64) (string, error
 
 	// get the path by generating a unique cache id
 	id := fmt.Sprintf("%d-%d", article, request)
-	path, err := filepath.Abs(filepath.Join(fs.path, "requests", id))
+	path, err := filepath.Abs(filepath.Join(fs.requestPath, id))
 	if err != nil {
 		return "", err
 	}
