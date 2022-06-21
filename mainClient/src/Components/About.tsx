10954ac04e97ba90bf1b65f498af4e7c40fb0bf3
Amy van der Meijden <A.vanderMeijden@student.tudelft.nl>
2022-06-22 01:28:08 +0200

    about page

---
+++ mainClient/src/Components/About.tsx
@@ -0,0 +1,22 @@
+import LoadingSpinner from "./LoadingSpinner";
+import ArticleListElement from "./Article/ArticleListElement";
+import * as React from "react";
+
+export default function NotificationAlert() {
+    return (
+        <div className="wrapper col-8 m-auto">
+            <div className={"accordion"}>
+                <p>This application is created by five second year Computer Science and Engineering students from TU Delft,
+                in the academic year 2021-2022:
+                </p>
+                <ul>
+                    <li>Amy van der Meijden</li>
+                    <li>Andreea Zlei</li>
+                    <li>Emiel Witting</li>
+                    <li>Jos Sloof</li>
+                    <li>Mattheo de Wit</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
