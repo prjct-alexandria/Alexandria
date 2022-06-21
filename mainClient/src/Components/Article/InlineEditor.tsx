8dce1f1013a81c10b1b9e955188c31e55d59465d
aandreea <A.Zlei@student.tudelft.nl>
2022-06-21 10:33:23 +0200

    Add tabs for view and edit modes

--- mainClient/src/Components/Article/InlineEditor.tsx
+++ mainClient/src/Components/Article/InlineEditor.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import * as React from "react";
+import FileUpload from "./FileUpload";
+import NotificationAlert from "../NotificationAlert";
 
 type EditorProps = {
   content: string;
@@ -8,6 +10,8 @@ type EditorProps = {
 
 export default function InlineEditor(props: EditorProps) {
   let [editorData, setEditorData] = useState<string>(props.content);
+  let [showSaveSuccessMessage, setSaveSuccess] = useState<boolean>(false);
+  let [editError, setEditError] = useState<Error>();
 
   let params = useParams();
 
@@ -27,11 +31,26 @@ export default function InlineEditor(props: EditorProps) {
       mode: "cors",
       credentials: "include",
       body: formData,
-    }).then((response) => {
-      if (response.ok) {
-        props.content = editorData;
+    }).then(
+      async (response) => {
+        if (response.ok) {
+          // props.content = editorData;
+          setSaveSuccess(true);
+          setTimeout(() => setSaveSuccess(false), 3000);
+        } else {
+          // Set error with message returned from the server
+          let responseJSON: {
+            message: string;
+          } = await response.json();
+
+          let serverMessage: string = responseJSON.message;
+          setEditError(new Error(serverMessage));
+        }
+      },
+      (error) => {
+        setEditError(error);
       }
-    });
+    );
   };
 
   const discardChanges = () => {
@@ -41,17 +60,22 @@ export default function InlineEditor(props: EditorProps) {
   // @ts-ignore
   return (
     <>
-      <nav className="nav d-grid gap-2 d-md-flex">
-        <button className={"nav-item"} type="button" onClick={saveChanges}>
-          Save changes
-        </button>
-        <button className={"nav-item"} type="button" onClick={discardChanges}>
-          Discard changes
-        </button>
-      </nav>
-      <div className="articleContent">
+      {showSaveSuccessMessage && (
+        <NotificationAlert
+          errorType="success"
+          title="Edits saved! "
+          message={"Your changes have been successfully saved."}
+        />
+      )}
+      {editError && (
+        <NotificationAlert
+          errorType="danger"
+          title={"Error: "}
+          message={"Something went wrong. " + editError}
+        />
+      )}
+      <div className="inline-editor-article">
         <textarea
-          style={{ whiteSpace: "pre-line" }}
           className="col-xs-12 textarea"
           value={editorData}
           onChange={(e) => {
@@ -59,6 +83,65 @@ export default function InlineEditor(props: EditorProps) {
           }}
         />
       </div>
+      <nav className="nav d-grid gap-2 d-md-flex">
+        <>
+          <button
+            type="button"
+            className="btn  btn-light"
+            data-bs-toggle="modal"
+            data-bs-target="#uploadFile"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-file-earmark-arrow-up"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8.5 11.5a.5.5 0 0 1-1 0V7.707L6.354 8.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 7.707V11.5z" />
+              <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+            </svg>
+            Upload File (overwrites current)
+          </button>
+          <FileUpload />
+        </>
+        <div className="flex-fill"></div>
+        <button
+          className={"nav-item btn btn-success "}
+          type="button"
+          onClick={saveChanges}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-check-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+          </svg>
+          Save changes
+        </button>
+        <button
+          className={"nav-item btn btn-light"}
+          type="button"
+          onClick={discardChanges}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-x"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+          </svg>
+          Discard changes
+        </button>
+      </nav>
     </>
   );
 }
--- mainClient/src/Components/NavigationBar.tsx
+++ mainClient/src/Components/NavigationBar.tsx
@@ -95,8 +95,22 @@ export default function NavigationBar() {
 
                 {isLoggedIn && (
                   <li className="nav-item dropdown">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="inline-block bi bi-person-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                      <path
+                        fill-rule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                      />
+                    </svg>
                     <a
-                      className="nav-link dropdown-toggle"
+                      className="nav-link dropdown-toggle inline-block"
                       href="#"
                       id="navbarDropdown"
                       role="button"
