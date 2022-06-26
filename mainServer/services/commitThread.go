2cb5c8a1f45e698a4129af0b9a67903f7a94e158
Mattheo de Wit <M.C.A.deWit@student.tudelft.nl>
2022-06-27 00:51:32 +0200

    Add commit(selection)thread service tests

--- mainServer/services/commitThread.go
+++ mainServer/services/commitThread.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"fmt"
 	"mainServer/models"
 	"mainServer/repositories/interfaces"
+	gitUtils "mainServer/utils/git"
 )
 
 type CommitThreadService struct {
@@ -10,11 +12,14 @@ type CommitThreadService struct {
 }
 
 func (serv CommitThreadService) StartCommitThread(cid string, tid int64) (int64, error) {
+	if !gitUtils.IsCommitHash(cid) {
+		return int64(-1), fmt.Errorf("invalid commit ID, got %s", cid)
+	}
 
 	// create commitThread
 	id, err := serv.CommitThreadRepository.CreateCommitThread(cid, tid)
 	if err != nil {
-		return 0, err
+		return int64(-1), err
 	}
 
 	return id, err
@@ -22,6 +27,10 @@ func (serv CommitThreadService) StartCommitThread(cid string, tid int64) (int64,
 
 // GetCommitThreads  gets the commit comment threads from the database, using the article id (aid) and commit id (cid)
 func (serv CommitThreadService) GetCommitThreads(aid int64, cid string) ([]models.Thread, error) {
+	if !gitUtils.IsCommitHash(cid) {
+		return nil, fmt.Errorf("invalid commit ID, got %s", cid)
+	}
+
 	threads, err := serv.CommitThreadRepository.GetCommitThreads(aid, cid)
 	if err != nil {
 		return nil, err
